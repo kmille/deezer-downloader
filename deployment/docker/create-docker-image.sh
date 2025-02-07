bea177fc23917410c770e0633945d22c30ae6130
kmille <github@androidloves.me>
2025-02-07 14:55:28 +0100

    Update Docker(hub) deployment scripts

--- deployment/docker/create-docker-image.sh
+++ deployment/docker/create-docker-image.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -eu
 
-VERSION="v$(poetry version -s)"
-echo sudo docker build --no-cache --force-rm . --file Dockerfile --tag kmille2/deezer-downloader
-echo sudo docker tag kmille2/deezer-downloader "kmille2/deezer-downloader:$VERSION"
-echo sudo docker tag kmille2/deezer-downloader kmille2/deezer-downloader:latest
+# This needs to be run from the project root directory!
+# ./deployment/docker/create-docker-image.sh
+
+DD_VERSION="v$(poetry version -s)"
+# get 'alpine3.21' from 'FROM python:3.12-alpine3.21 AS builder'
+ALPINE_VERSION=$(tac Dockerfile | grep -m 1 ^FROM | cut -d - -f 2)
+
+sudo docker build --no-cache --force-rm . --file Dockerfile --tag kmille2/deezer-downloader
+sudo docker tag kmille2/deezer-downloader "kmille2/deezer-downloader:$DD_VERSION-$ALPINE_VERSION"
+sudo docker tag kmille2/deezer-downloader kmille2/deezer-downloader:latest
